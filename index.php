03efb20f8119185d37a2e1bf644f63a49f701493
tictaktech <37363115+tictaktech@users.noreply.github.com>
2018-03-25 22:13:51 +1100

    update 25/03/2018

---
+++ index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link href='https://fonts.googleapis.com/css?family=Roboto:100,300,400' rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:900" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/animate.css@3.5.2/animate.min.css">
+	<link rel="stylesheet" type="text/css" href="stylesheet.css">
+	<title>Tic Tak Tech</title>
+</head>
+<body>
+	<nav class="navbar navbar-collapse navbar-expand-md bg-light navbar-light sticky-top">
+	  <!-- Brand/logo -->
+	  	<a class="navbar-brand" href="index.php">
+	    	<img src="assets/logo_final.png" alt="logo" style="width: 80px;">
+	  	</a>	
+	  	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    		<span class="navbar-toggler-icon"></span>
+  		</button>
+	  <!-- Links -->
+	  	<div class="collapse navbar-collapse" id="collapsibleNavbar">
+	  		<ul class="navbar-nav mr-auto">
+			   <!--  <li class="nav-item">
+			      <a class="nav-link" href="#">About</a>
+			    </li>
+			    <li class="nav-item">
+			      <a class="nav-link" href="#">Services</a>
+			    </li> -->
+			    <li class="nav-item">
+			      <a class="nav-link" href="contactus.php">CONNECT</a>
+			    </li>
+	  		</ul>
+	  		<a href="http://www.facebook.com/tictaktech" target="_blank"><img style="width: 60px;" src="assets/icons/fb_icon.png"></a>
+	  		<a href="https://www.instagram.com/tictaktech/" target="_blank"><img style="margin-left:10px; width: 48px;" src="assets/icons/instagram_icon.png"></a>
+	  		
+	  	</div>
+	  	
+	</nav>
+	<div class="container-fluid sub-heading">
+		<div class="row slogan">
+			<div class="col-md-12">
+				<p class="connect-tech-text animated bounce">CONNECT TO A TECH</p>
+			</div>
+		</div>
+		<div class="row aim">
+			<div class="col-md-2"></div>
+			<div class="col-md-8">
+				<p class="animated lightSpeedIn aim-text ">"Our aim is not just to resolve your technical problems, but to connect you to a technician who can listen, correct, teach and develop your skills to become more confident in our technological world."</p>
+			</div>
+			<div class="col-md-2"></div>	
+		</div>
+		<div class="row sales-service-repairs">
+			<div class="col-md-4">
+				<p class="animated bounce">SALES</p>
+			</div>
+			<div class="col-md-4">
+				<p class="animated bounce">SERVICE</p>
+			</div>
+			<div class="col-md-4">
+				<p class="animated bounce">REPAIRS</p>
+			</div>
+		</div>
+		<div class="row service-list">
+			<div class="col-6 col-md-2">
+				<img class="rounded-circle animated bounce" src="assets/icons/desktop.png">
+				<p>Desktops</p>
+			</div>
+			<div class="col-6 col-md-2">
+				<img class="rounded-circle animated bounce" src="assets/icons/laptop.png">
+				<p>Notebooks</p>
+			</div>
+			<div class="col-6 col-md-2">
+				<img class="rounded-circle animated bounce" src="assets/icons/tablet.png">
+				<p>Tablets</p>
+			</div>
+			<div class="col-6 col-md-2">
+				<img class="rounded-circle animated bounce" src="assets/icons/printer.png">
+				<p>Printers</p>
+			</div>
+			<div class="col-6 col-md-2">
+				<img class="rounded-circle animated bounce" src="assets/icons/router.png">
+				<p>Networking</p>
+			</div>
+			<div class="col-6 col-md-2">
+				<img class="rounded-circle animated bounce" src="assets/icons/entertainment.png">
+				<p>Home Entertainment</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="container-fluid grid">	
+		<div class="row">
+			<div class="col-md-5 grid-child">
+				<div style="text-align: center;" class="grid-content">
+					<p class="sub-heading-text">Why Tic Tak Tech?</p>
+					<p class="body-text">&#x2714; Friendly, experienced and knowledgable technicans!</p>
+					<p class="body-text">&#x2714; Pay for SERVICE instead of TIME!</p>
+					<p class="body-text">&#x2714; $90 Flat-rate Callout!</p>
+					<p class="body-text">&#x2714; NO FIX, NO FEE, Guarenteed!*</p>
+					<a href="contactus.php"><button type="button" class="contact-button btn">Contact Us</button></a>
+					<img class="loading-drivers-img" src="assets/images/loading_drivers.jpg">
+				</div>
+				
+			</div>
+			<div class="col-md-7 grid-child">
+				<div class="grid-content">
+					<p class="sub-heading-text">Servicing the South-Eastern Suburbs</p>
+					<img src="assets/map.png" class="main-map">
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
